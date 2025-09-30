925a65cdb7708d3fe5552c1e39b0edfddc2cde29
egemencakir39 <115990783+egemencakir39@users.noreply.github.com>
2025-09-30 18:53:59 +0300

    Update SearchModal.jsx
    Search eklendi

--- BlogWebSite/blog-website/src/components/SearchModal.jsx
+++ BlogWebSite/blog-website/src/components/SearchModal.jsx
@@ -1,9 +1,24 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { use } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "@/redux/productSlice";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const SearchModal = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const { items, loading, error } = useSelector((state) => state.products);
+  const safeItems = Array.isArray(items) ? items : [];
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredItems = safeItems.filter((products) =>
+    products.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
   return (
-  <div className="fixed inset-0  bg-black/80  z-50 flex items-center justify-center">
+    <div className="fixed inset-0  bg-black/80  z-50 flex items-center justify-center">
       <div className=" bg-white  p-6 rounded-lg w-full max-w-md shadow-xl relative">
         <button
           onClick={onClose}
@@ -17,18 +32,31 @@ const SearchModal = ({ onClose }) => {
           type="text"
           placeholder="Başlığa göre ara..."
           className="w-full border px-3 py-2 rounded-md focus:ring-2 focus:ring-blue-500"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
 
-        {/* Arama sonuçları (dummy) */}
+        {/* Arama sonuçları */}
         <ul className="mt-4">
-          <li className="py-1 border-b">Blog 1</li>
-          <li className="py-1 border-b">Blog 2</li>
+          {filteredItems.length === 0 ? (
+            <p className="text-gray-500">Sonuç bulunamadı.</p>
+          ) : (
+            filteredItems.map((product) => (
+              <Link
+                onClick={onClose}
+                href={`/blog/${product._id}`}
+                key={product._id}
+              >
+                <li className="py-2 border-b cursor-pointer">
+                  {product.title}
+                </li>
+              </Link>
+            ))
+          )}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchModal
-
-
+export default SearchModal;
